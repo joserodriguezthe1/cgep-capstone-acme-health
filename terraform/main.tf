5906ba8a0a45e2f3a0e0087bdb31b1c67fe593e9
Jose Rodriguez <jose.a.rodriguez14@hotmail.com>
2026-05-12 17:04:11 -0400

    Add S3 backend for persistent Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,11 @@
 
 terraform {
   required_version = ">= 1.6"
+  backend "s3" {
+    bucket = "acme-health-tfstate-758742551881"
+    key    = "capstone/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws     = { source = "hashicorp/aws", version = "~> 5.0" }
     random  = { source = "hashicorp/random", version = "~> 3.6" }

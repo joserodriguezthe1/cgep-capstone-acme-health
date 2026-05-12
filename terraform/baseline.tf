91212f36b6fe5a9717f7a1daef508b7b5143f4df
Jose Rodriguez <jose.a.rodriguez14@hotmail.com>
2026-05-12 07:32:08 -0400

    Green branch: Apply CMMC baseline fixes - close GAP-01 through GAP-07

--- terraform/baseline.tf
+++ terraform/baseline.tf
@@ -6,7 +6,7 @@
 # CMMC SC.L2-3.13.11
 ######################################################################
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "uploads" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "uploads" {
   bucket = aws_s3_bucket.uploads.id
   rule {
     apply_server_side_encryption_by_default {

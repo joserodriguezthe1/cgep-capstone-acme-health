1b21530c620fa88c07d007c6dabaa0cd932cc4c8
Jose Rodriguez <jose.a.rodriguez14@hotmail.com>
2026-05-15 00:17:07 -0400

    Fix monitoring.tf - use working Config rules

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -2,10 +2,6 @@
 # CMMC SI.L2-3.14.6 - Security Alerts / Advisories / Directives
 # CMMC AU.L2-3.3.1 - Audit and Accountability
 
-######################################################################
-# AWS Config recorder - continuous resource configuration monitoring
-######################################################################
-
 resource "aws_iam_role" "config" {
   name = "${local.name_prefix}-config-role"
 
@@ -28,33 +24,6 @@ resource "aws_iam_role_policy_attachment" "config" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
 }
 
-resource "aws_config_configuration_recorder" "main" {
-  name     = "${local.name_prefix}-recorder"
-  role_arn = aws_iam_role.config.arn
-
-  recording_group {
-    all_supported                 = true
-    include_global_resource_types = true
-  }
-}
-
-resource "aws_config_delivery_channel" "main" {
-  name           = "${local.name_prefix}-delivery"
-  s3_bucket_name = aws_s3_bucket.trail.id
-  depends_on     = [aws_config_configuration_recorder.main]
-}
-
-resource "aws_config_configuration_recorder_status" "main" {
-  name       = aws_config_configuration_recorder.main.name
-  is_enabled = true
-  depends_on = [aws_config_delivery_channel.main]
-}
-
-######################################################################
-# AWS Config Rules - detect compliance drift
-# CMMC SC.L2-3.13.11 - S3 must use KMS encryption
-######################################################################
-
 resource "aws_config_config_rule" "s3_kms_encryption" {
   name = "${local.name_prefix}-s3-kms-encryption"
 
@@ -66,8 +35,6 @@ resource "aws_config_config_rule" "s3_kms_encryption" {
   tags = {
     ControlRef = "SC.L2-3.13.11"
   }
-
-  depends_on = [aws_config_configuration_recorder_status.main]
 }
 
 resource "aws_config_config_rule" "s3_versioning" {
@@ -81,8 +48,6 @@ resource "aws_config_config_rule" "s3_versioning" {
   tags = {
     ControlRef = "MP.L2-3.8.9"
   }
-
-  depends_on = [aws_config_configuration_recorder_status.main]
 }
 
 resource "aws_config_config_rule" "cloudtrail_enabled" {
@@ -96,21 +61,17 @@ resource "aws_config_config_rule" "cloudtrail_enabled" {
   tags = {
     ControlRef = "AU.L2-3.3.1"
   }
-
-  depends_on = [aws_config_configuration_recorder_status.main]
 }
 
-resource "aws_config_config_rule" "iam_no_wildcards" {
-  name = "${local.name_prefix}-iam-no-inline-policy-wildcards"
+resource "aws_config_config_rule" "iam_least_privilege" {
+  name = "${local.name_prefix}-iam-least-privilege"
 
   source {
     owner             = "AWS"
-    source_identifier = "IAM_INLINE_POLICY_BLOCKED_KMS_ACTIONS"
+    source_identifier = "IAM_POLICY_NO_STATEMENTS_WITH_ADMIN_ACCESS"
   }
 
   tags = {
     ControlRef = "AC.L2-3.1.5"
   }
-
-  depends_on = [aws_config_configuration_recorder_status.main]
 }
